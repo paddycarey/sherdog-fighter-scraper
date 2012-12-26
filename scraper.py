570c28c4d49f3ec17e4fe53b963fb5c1c4f4c780
Patrick Carey <paddy@wackwack.co.uk>
2012-12-26 21:33:21 +0000

    Fucking smart quotes

--- scraper.py
+++ scraper.py
@@ -153,7 +153,10 @@ class UnicodeWriter:
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
+        try:
+            self.writer.writerow([s.encode("utf-8") for s in row])
+        except UnicodeDecodeError:
+            self.writer.writerow(row)
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
         data = data.decode("utf-8")
